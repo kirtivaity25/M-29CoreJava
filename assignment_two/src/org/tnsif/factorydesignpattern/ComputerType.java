3e4bd0cdb95d3f4030df933baf2635208f9bb182
kirtivaity25 <kiivaity2520@gmail.com>
2023-05-23 15:40:38 +0530

    first commit

---
+++ assignment_two/src/org/tnsif/factorydesignpattern/ComputerType.java
@@ -0,0 +1,9 @@
+package org.tnsif.factorydesignpattern;
+
+/**
+ * Types of Computers Created by ComputerFactory
+ * 
+ */
+public enum ComputerType {
+	PC, LAPTOP, SERVER;
+}
---
+++ assignment_two/src/org/tnsif/singletonpattern/StaticBlock.java
@@ -0,0 +1,28 @@
+
+
+//program on demonstrate to singleton pattern using static block initialization
+
+package org.tnsif.singletonpattern;
+public class StaticBlock {
+	//create private constructor
+		private static StaticBlock INSTANCE = new StaticBlock();
+		
+		//create private static variable of the same class
+		StaticBlock() {
+			
+		}
+		//in static block it will give the exception handling to handle errors
+		static {
+			try {
+				if(INSTANCE == null) {
+					INSTANCE = new StaticBlock();
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		//create public static method that returns instance of the class
+		public static StaticBlock getInstance() {
+			return INSTANCE;
+		}
+}
