3c1bc17311fe7190ba56547e1976dae3f7ef27c8
kirtivaity25 <kiivaity2520@gmail.com>
2023-05-23 15:11:12 +0530

    first Commit

---
+++ daynine/src/daynine/ArrayOfObjects.java
@@ -0,0 +1,18 @@
+package daynine;
+
+public class ArrayOfObjects {
+
+	public static void main(String[] args) {
+		Customer[] arr = new Customer[2]; //array of objects
+		
+		arr[0] = new Customer(123, "kirti");
+		arr[1] = new Customer(456, "Vaishnavi");
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i].getId()+" "+arr[i].getName());
+		}
+		
+
+	}
+
+}
