3740377cec769a2f3a71312b03a0380b7c2b5d49
kirtivaity25 <kiivaity2520@gmail.com>
2023-05-23 15:01:25 +0530

    First Commit

---
+++ dayseven/src/org/tnsif/thiskeyword/ThisKeywordExecuter.java
@@ -0,0 +1,12 @@
+package org.tnsif.thiskeyword;
+
+public class ThisKeywordExecuter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			   ThisKeyword t  = new ThisKeyword(2323,123);
+			   t.print();
+	}
+
+}
