0a34061e81623bcd756d8d34438f5b78367dbd08
kirtivaity25 <kiivaity2520@gmail.com>
2023-06-20 13:29:34 +0530

    First Commit

---
+++ dayseventeen/src/org/tnsif/datetimeapi/ClockClassExample.java
@@ -0,0 +1,17 @@
+package org.tnsif.datetimeapi;
+
+//program to demonstrate on clock class
+import java.time.Clock;
+
+public class ClockClassExample {
+
+	public static void main(String[] args) {
+		Clock c1 = Clock.systemUTC();
+		System.out.println(c1);
+		
+		Clock c2 = Clock.systemDefaultZone();
+		System.out.println(c2);
+
+	}
+
+}
